a92b627c0373507fe994262c26699bac5c8977cc
Srivatsa <srivatsa.workspace@gmail.com>
2024-09-04 21:32:27 +0530

    updated compose and init.sql file

--- init.sql
+++ init.sql
@@ -1,6 +1,10 @@
+-- Create the database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS testdb;
+
+-- Switch to the created database
 USE testdb;
 
+-- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
